01de75fff6c0658a125b3108e03e8d7c2554d3ba
nandini-dey <dey.hiya16@gmail.com>
2022-04-20 11:52:15 +0530

    commit on main

---
+++ js/login-page.js
@@ -0,0 +1,15 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "user" && password === "abc123") {
+        window.location.assign("index.html");
+    } else {
+        alert("ERROR!! \nyou've Username or password entered")
+    }
+})
